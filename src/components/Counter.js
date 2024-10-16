70c475321ea49317cc994a448ea7791188a7bb46
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-16 23:49:22 +0300

    changing component Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Button from '@mui/material/Button'
 
 export default function Counter() {
   return (
-    <div>
-      <Button variant="contained">Contained</Button>
-      <Button variant="contained">Contained</Button>
+    <div style={{display:'flex', justifyContent:'center', alignItems:'center', minHeight:'100vh'}}>
+      <Button variant="contained">+</Button>
+      <span>0</span>
+      <Button variant="contained">-</Button>
     </div>
   )
 }
