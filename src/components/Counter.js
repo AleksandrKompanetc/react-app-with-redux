e4f71784b5502f45c77e5cc9c21169fc0cab05bd
AleksandrKompanetc <moneymacker7@gmail.com>
2024-10-16 23:07:26 +0300

    adding component Counter

---
+++ src/components/Counter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Counter() {
+  return (
+    <div>
+      <Button variant="contained">Contained</Button>
+      <Button variant="contained">Contained</Button>
+    </div>
+  )
+}
